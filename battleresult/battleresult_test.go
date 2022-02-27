926e27d5a91564ed3b31ef4d4a7cae6f031d0ea0
Ahmed Saeed <Ahmed.Saeed@teachersoftomorrow.org>
2022-02-27 16:37:42 -0600

    test for battle result

--- battleresult/battleresult_test.go
+++ battleresult/battleresult_test.go
@@ -1,6 +1,7 @@
 package battleresult
 
 import (
+	"bytes"
 	"math/rand"
 	"testing"
 	"time"
@@ -14,7 +15,7 @@ func BenchmarkAdd(b *testing.B) {
 	// run the Fib function b.N times
 	rand.Seed(time.Now().Unix())
 
-	x := New()
+	br := New()
 	temp := rand.Perm(19)
 	available := make([]uint8, 19)
 	for i, x := range temp {
@@ -24,13 +25,49 @@ func BenchmarkAdd(b *testing.B) {
 	skipIndexes := []uint8{1, 5}
 	b.ResetTimer()
 	for t := 0; t < b.N; t++ {
-		x.Configure(17)
+		br.Configure(17)
 
 		for n := 0; n < 7000; n++ {
-			x.Add(available, skipIndexes, n)
+			br.Add(available, skipIndexes, n)
 		}
-		_, tieCount, _ := x.Next()
+		_, tieCount, _ := br.Next()
 
 		availableglobal = append(availableglobal, tieCount)
+	}
+}
+
+func TestBattleResutl(t *testing.T) {
+
+	br := New()
+	br.Configure(4)
+
+	br.Add([]uint8{1, 2, 3, 4, 5, 6}, []uint8{0, 1}, 2)
+	br.Add([]uint8{1, 2, 3, 4, 5, 6}, []uint8{1, 2}, 3)
+	br.Add([]uint8{1, 2, 3, 4, 5, 6}, []uint8{3, 4}, 4)
+	br.Add([]uint8{1, 2, 3, 4, 5, 6}, []uint8{0, 5}, 5)
+
+	testNext(t, &br, []uint8{3, 4, 5, 6}, 2, false)
+	testNext(t, &br, []uint8{1, 4, 5, 6}, 3, false)
+	testNext(t, &br, []uint8{1, 2, 3, 6}, 4, false)
+	testNext(t, &br, []uint8{2, 3, 4, 5}, 5, false)
+	testNext(t, &br, nil, 0, true)
+	// expectedTC :=
+
+}
+
+func testNext(t *testing.T, br *BattleResult, expectedAvailable []uint8, expectedTieCount int, expectedDone bool) {
+
+	available, tieCount, done := br.Next()
+
+	if done != expectedDone {
+		t.Errorf("expected %v got %v", expectedDone, done)
+	}
+
+	if !bytes.Equal(available, expectedAvailable) {
+		t.Errorf("expected %v got %v", expectedAvailable, available)
+	}
+
+	if tieCount != expectedTieCount {
+		t.Errorf("expected %v got %v", expectedTieCount, tieCount)
 	}
 }
